17f67c07853e1f85c384d2999b3acc51063fd584
Charly B. <charlyxxxddd@gmail.com>
2025-10-14 10:42:22 +0200

    unterseiten sind schonmal da, aber keine Funktion

---
+++ MealMate-App/src/app/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  imports: [],
+  templateUrl: './shopping-list.html',
+  styleUrl: './shopping-list.css'
+})
+export class ShoppingList {
+
+}
