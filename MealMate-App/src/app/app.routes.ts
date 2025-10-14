e13d3a47660bb98990db55046d79c51244691ca8
Patryk <giffygang.business@gmail.com>
2025-10-14 09:27:02 +0200

    Routes added Login & Register

--- MealMate-App/src/app/app.routes.ts
+++ MealMate-App/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+
+export const routes: Routes = [
+
+    {
+        path : '',
+        redirectTo: 'app',
+        pathMatch: 'full'
+    },
+    {
+        path : 'login',
+        component: Login
+    },
+    {
+        path : 'register',
+        component: Register
+    }
+
+];
