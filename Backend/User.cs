47a0a09e686eea66e5e4e5f6040996e119938b75
a913171 <adrian.retzer@eviden.com>
2025-10-15 16:09:15 +0200

    Aufteilung von Front- und Backend

--- Backend/User.cs
+++
@@ -1,8 +0,0 @@
-public class User
-{
-    public int UserId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PwHash { get; set; }
-}
