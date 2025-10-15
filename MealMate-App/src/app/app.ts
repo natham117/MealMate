5668b9e24a2842f8e235fed3fba60cbc9d9f90fd
Charly B. <charlyxxxddd@gmail.com>
2025-10-15 11:47:06 +0200

    Register und Login geht jetzt

--- MealMate-App/src/app/app.ts
+++ MealMate-App/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterModule],
